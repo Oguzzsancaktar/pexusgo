11318fb630bd337ee0a9183f235bc419e6a0c611
oguzsancaktar <osncktr.psd@outlook.com.tr>
2024-08-26 22:31:34 +0300

    first commit

---
+++ src/app/(server-components)/SectionHero.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from 'react';
+import imagePng from '@/images/hero-right.png';
+import HeroSearchForm from '../(client-components)/(HeroSearchForm)/HeroSearchForm';
+import Image from 'next/image';
+import ButtonPrimary from '@/shared/ButtonPrimary';
+
+export interface SectionHeroProps {
+  className?: string;
+}
+
+const SectionHero: FC<SectionHeroProps> = ({ className = '' }) => {
+  return (
+    <div
+      className={`nc-SectionHero flex flex-col-reverse lg:flex-col relative ${className}`}
+    >
+      <div className="flex flex-col lg:flex-row lg:items-center">
+        <div className="flex-shrink-0 lg:w-1/2 flex flex-col items-start space-y-8 sm:space-y-10 pb-14 lg:pb-64 xl:pr-14 lg:mr-10 xl:mr-0">
+          <h2 className="font-medium text-4xl md:text-5xl xl:text-7xl !leading-[114%] ">
+            Vip Havalimanı Transferi
+          </h2>
+          <span className="text-base md:text-lg text-neutral-500 dark:text-neutral-400">
+            İstanbul havalimanı transfer ve taksi hizmetlerinde misafirlerimizin
+            en çok tercih ettiği lokasyonları sizler için listeledik. istanbul
+            havalimanı, sabiha gökçen havalimanı, istanbul otelleri veya
+            alışveriş merkezlerine seyahat edecekseniz bir göz atın.
+          </span>
+          <ButtonPrimary href="/listing-stay-map" sizeClass="px-5 py-4 sm:px-7">
+            Turları Görüntüle
+          </ButtonPrimary>
+        </div>
+        <div className="flex-grow">
+          <Image className="w-full" src={imagePng} alt="hero" priority />
+        </div>
+      </div>
+
+      <div className="hidden lg:block z-10 mb-12 lg:mb-0 lg:-mt-40 w-full">
+        <HeroSearchForm />
+      </div>
+    </div>
+  );
+};
+
+export default SectionHero;
